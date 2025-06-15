ec1897dde23650582ea632b24d0cadde733879e9
Jasmin Kıcıman <yaseminkcman@gmail.com>
2025-06-16 00:36:59 +0300

    Add gitignore and first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ad: Jasmin");
+        Console.WriteLine("Soyad: Kıcıman");
+        Console.WriteLine("Patika Bootcamp'e Katılma Nedeni: Yazılım alanında kendimi geliştirip profesyonel bir full-stack developer olmak istiyorum.");
+    }
+}
